a5e349e72c11f44db11c35e4bf5e5c011b76e6e8
makwaje <alloushi2010@gmail.com>
2024-10-31 22:08:13 +0300

    init2

--- src/lib/supabase/client.js
+++ src/lib/supabase/client.js
@@ -2,7 +2,7 @@ import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
   return createBrowserClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
+    "https://huiamspnzkmehxtfzkrs.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1aWFtc3BuemttZWh4dGZ6a3JzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk3NjgzMTgsImV4cCI6MjA0NTM0NDMxOH0.2QU_aExTTNi4O8nJmIyyNv3LN5fUNVNIr2-_2rf7xcU"
   );
 }
--- src/lib/supabase/server.js
+++ src/lib/supabase/server.js
@@ -5,8 +5,8 @@ export async function createClient() {
   const cookieStore = await cookies();
 
   return createServerClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY,
+    "https://huiamspnzkmehxtfzkrs.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1aWFtc3BuemttZWh4dGZ6a3JzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk3NjgzMTgsImV4cCI6MjA0NTM0NDMxOH0.2QU_aExTTNi4O8nJmIyyNv3LN5fUNVNIr2-_2rf7xcU",
     {
       cookies: {
         getAll() {
